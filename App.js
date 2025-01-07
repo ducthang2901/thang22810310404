00faba7054a65c17c816602181b93077ef1dbaa1
ducthang2901 <anhtretxtq@gmail.com>
2025-01-07 10:54:24 +0700

    first

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default App;
